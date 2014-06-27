b6ee947c56abb99441db49c8751471ccebabc4c4
Arne Bech <arne@datapad.io>
2014-06-27 14:35:46 -0700

    fix user setting

--- settings.global.sh
+++ settings.global.sh
@@ -11,13 +11,16 @@ GITHUB_USERNAME="No user name set"
 
 ## OTHER
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 RED='\033[00;31m'
 END_COLOR='\033[00m'
 
-if [ -f settings.user.sh ]; then
-    source settings.user.sh
+user_settings="${SCRIPT_DIR}/settings.user.sh"
+
+if [ -f "$user_settings" ]; then
+    source "$user_settings"
 else
-    dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     echo -e "${RED}Please set up 'settings.user.sh' in '${dir}' with your settings${END_COLOR}"
     exit
 fi
