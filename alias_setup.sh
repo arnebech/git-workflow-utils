e3a84f7725592f10086305140d13910e17aab4cf
Arne Bech <arne@datapad.io>
2014-07-01 08:42:28 -0700

    prefix aliases and make first letter unique

--- alias_setup.sh
+++ alias_setup.sh
@@ -3,9 +3,11 @@
 #
 # source this script from profile for easy setup
 
+shopt -s expand_aliases
+
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-alias create_feature_branch="${dir}/create_feature_branch.sh"
-alias try_pr="${dir}/try_pr.sh"
-alias rebase_from_dev="${dir}/rebase_from_develop.sh"
-alias create_pr="${dir}/create_pr.sh"
+alias gf_feature_branch="${dir}/create_feature_branch.sh"
+alias gf_try_pr="${dir}/try_pr.sh"
+alias gf_rebase_from_dev="${dir}/rebase_from_develop.sh"
+alias gf_create_pr="${dir}/create_pr.sh"
